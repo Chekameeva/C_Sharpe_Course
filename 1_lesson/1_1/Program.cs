d2cc9dee090988754ead15cd95cd0325dc3aa56f
Chekameeva <anastasiya-parsek@mail.ru>
2022-11-22 17:42:39 +0300

    Task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишиет программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Write a number1: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number2: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1 * num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
