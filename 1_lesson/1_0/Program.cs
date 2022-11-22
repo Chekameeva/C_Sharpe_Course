6dce07006a41dcc73b20f0ed040d1d95b6300467
Chekameeva <anastasiya-parsek@mail.ru>
2022-11-22 16:12:48 +0300

    Task 1_0

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишиет программу, которая на вход принимает число и 
+// выдаёт его квадрат (число умноженное само на себя).
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num * num);
