5b0ecd6b8c9cb9c90d7f1d77786a1ac01e834fef
Chekameeva <anastasiya-parsek@mail.ru>
2022-11-22 19:18:51 +0300

    Task 1_3

---
+++ 1_lesson/1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа от -N до N
+
+Console.WriteLine("Write number: ");
+
+string num = Console.ReadLine();
+int N = int.Parse(num);
+int NegN = - N;
+
+while(NegN <= N)
+{
+    Console.Write($"{NegN} ");
+    NegN++;
+}
+
