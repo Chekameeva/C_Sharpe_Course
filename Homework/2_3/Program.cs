99924fdc409024c9ebd99d3579830c7ccfba0f4e
Chekameeva <anastasiya-parsek@mail.ru>
2022-11-25 14:16:54 +0300

    Homework Task 2_3

--- Homework/2_3/Program.cs
+++ Homework/2_3/Program.cs
@@ -3,8 +3,12 @@
 
 string Weekend(int num)
 {
-    if (num == 6 || 7)
+    if (num == 6 || num == 7)
     return "Выходной день!";
     else
     return "Будний день";
 }
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Weekend(number));
