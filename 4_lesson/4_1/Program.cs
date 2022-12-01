35a127dce156364f1fa84b26478d9409349e8908
Chekameeva <anastasiya-parsek@mail.ru>
2022-12-01 20:48:46 +0300

    Task 4_0

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+
+int SumNums(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
