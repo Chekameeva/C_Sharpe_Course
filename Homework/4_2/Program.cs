14000f1aa0d52f228b43221052956c141476bde9
Chekameeva <anastasiya-parsek@mail.ru>
2022-12-01 23:06:00 +0300

    Homework task 4_1

---
+++ Homework/4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int SumNums(int num)
+{    
+        num = num % 10;
+        return num;
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumNums(n));
